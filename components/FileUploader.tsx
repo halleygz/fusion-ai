262d0b6b73895bd84898a2c84546e242460e5218
Halleluya Mulugeta <halleluya2013idone@gmail.com>
2024-08-16 13:17:18 +0300

    hot fix for fileuploader

--- components/FileUploader.tsx
+++ components/FileUploader.tsx
@@ -32,7 +32,7 @@ function FileUploader() {
           ) : (
             <>
               <CircleArrowDown className="h-20 w-20 animate-bounce" />
-              <p>Drag 'n' drop some files here, or click to select files</p>
+              <p>Drag n drop some files here, or click to select files</p>
             </>
           )}
         </div>
